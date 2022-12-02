b92668d1a002cf2d24ed36fb980921e478e47f05
Joe Burton <joeburton@gmail.com>
2022-12-02 11:57:49 +0000

    clean up

--- api/mongoUtilities.js
+++ api/mongoUtilities.js
@@ -1,23 +1,16 @@
 import mongodb from 'mongodb';
-import config from 'config';
+import 'dotenv/config';
 
 const { MongoClient } = mongodb;
 
-const production = config.env === 'production' ? true : false;
-
 let _db;
 
-console.log('Mongo DB Name: ', process.env.DB_NAME, process.env.DB_PASSWORD);
-console.log('config: ', config);
-
-let url = production
-  ? `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.dhtvx.mongodb.net/directory?retryWrites=true&w=majority`
-  : 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0';
+const DB_CONNECTION_STRING = process.env.MONGODB || '';
 
 const mongoUtilities = {
   connectToDatabase(callback) {
     MongoClient.connect(
-      url,
+      DB_CONNECTION_STRING,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
